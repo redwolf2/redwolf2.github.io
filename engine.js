95479eda0a4c2dd5202bd7168b2ea88bd25bcd6f
fdsa fdsa <redwolf2@gmx.de>
2017-08-24 01:40:18 +0200

    Added save and load functionality

--- engine.js
+++ engine.js
@@ -1,54 +1,9 @@
-var globalChoices
-
-var createChoices = function(choices) {
-    var ret = ""
-    choices.forEach(function(choice) {
-        ret += (choice.create() + "<br/><br/>")
-    })
-    return ret;
-}
-
-var createButton = function(id, text, func) {
-    return "<input type=\"button\" id=\"" + id + "\", name=\"button\" class=\"btn continue\", onclick=\"onButton(" + id + ")\", value=\"" + text +  "\" />"
-}
-
-var addText = function(text) {
-    document.getElementById("text").innerHTML = text;
-}
-
-var appendText = function(text) {
-    document.getElementById("text").innerHTML += text;
-}
-
-var clearText = function() {
-    addText("")
-}
-
-var addBottom = function(choices) {
-    globalChoices = choices
-    var e = document.getElementById("center")
-    if(choices === undefined) {
-        e.innerHTML = ""
-    } else if(choices.length > 1) {
-        var counter = 0;
-        choices.forEach(function(choice) {
-            counter++;
-            choice.id = "choice" + counter
-        })
-        e.innerHTML = createChoices(choices) 
-        choices.forEach(function(choice) {
-            choice.bind()
-        })
-        e.innerHTML += createButton("confirm", "Weiter", function() {})
-    } else if(choices.length === 1) {
-        e.innerHTML = createButton(choices[0].id, "Weiter", function() {})
-    }
-}
+var gamestate
 
 var put = function(name, value) {
     if (typeof(Storage) !== "undefined") {
         var valueS = JSON.stringify(value)
-        localStorage.setItem(name, valueS);
+        localStorage.setItem(name, valueS)
         console.log("setting var named " + name + " to " + valueS)
     } else {
         throw "Sorry, your browser does not support Web Storage..."
@@ -65,26 +20,85 @@ var get = function(name, defaultValue) {
 }
 
 function getRnd(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min) ) + min
 }
 
-var getSelectedChoice = function() {
+function Engine(customState)
+{
+    gamestate = customState
+    this.globalChoices
+
+    var appendText = function(text) {
+        document.getElementById("text").innerHTML += text
+    }
+
+    var clearText = function() {
+        addText("")
+    }
+}
+
+Engine.show = function(text, choices, save = true) {
+    Engine.addText(text)
+    Engine.addBottom(choices)
+    gamestate.event = new GameEvent(text, choices)
+    if(save)
+        GameState.save()
+}
+
+Engine.createChoices = function(choices) {
+    var ret = ""
+    choices.forEach(function(choice) {
+        ret += (Choice.create(choice) + "<br/><br/>")
+    })
+    return ret
+}
+
+Engine.addBottom = function(choices) {
+    globalChoices = choices
+    var e = document.getElementById("center")
+    if(choices === undefined) {
+        e.innerHTML = ""
+    } else if(choices.length > 1) {
+        var counter = 0
+        choices.forEach(function(choice) {
+            counter++
+            choice.id = "choice" + counter
+        })
+        e.innerHTML = Engine.createChoices(choices) 
+        choices.forEach(function(choice) {
+            Choice.bind(choice)
+        })
+        e.innerHTML += Engine.createButton("confirm", "Weiter", function() {})
+    } else if(choices.length === 1) {
+        e.innerHTML = Engine.createButton(choices[0].id, "Weiter", function() {})
+    }
+}
+
+Engine.createButton = function(id, text, func) {
+    return "<input type=\"button\" id=\"" + id + "\", name=\"button\" class=\"btn continue\", onclick=\"Engine.onButton(" + id + ")\", value=\"" + text +  "\" />"
+}
+
+Engine.addText = function(text) {
+    document.getElementById("text").innerHTML = text
+}
+
+Engine.getSelectedChoice = function() {
     var selectedId = undefined
     if(globalChoices.length > 1) {
         var choices = document.getElementsByName("choice")
         choices.forEach(function(choice) {
             if(choice.checked) {
-                selectedId = choice.id;
+                selectedId = choice.id
             }
         })
     } else if(globalChoices.length === 1) {
         selectedId = globalChoices[0].id
     }
-    return selectedId;
+    return selectedId
 }
 
-var onButton = function(button) {
-    var selectedId = getSelectedChoice()
+Engine.onButton = function(button) {
+    var selectedId = Engine.getSelectedChoice()
     var selectedChoice
     globalChoices.forEach(function(choice) {
         if(choice.id === selectedId) {
@@ -93,20 +107,37 @@ var onButton = function(button) {
     })
     if(selectedChoice != undefined) {
         console.log(selectedChoice.text + " was selected.")
-        selectedChoice.func()
-        window.scrollTo(0, 0);
+        window[selectedChoice.func](arguments)
+        window.scrollTo(0, 0)
     }
+}
+
+function GameState() {
+    this.event = new GameEvent("", [new Choice("Weiter", "event404")])
+}
+
+GameState.save = function() {
+    put("gamestate", gamestate)
+}
+GameState.load = function() {
+    gamestate = get("gamestate")
+    Engine.show(gamestate.event.text, gamestate.event.choices)
+}
+
+function GameEvent(text, choices) {
+    this.text = text
+    this.choices = choices
 }
 
 function Choice(text, func) {
     this.id = "choice0"
     this.text = text
     this.func = func
-    
-    this.create = function() {
-        return "<label id=\"" + this.id + "label\" for=\"" + this.id + "\"><input type=\"radio\" id=\"" + this.id + "\", name=\"choice\" />" + this.text + "</label>"
-    }
-    this.bind = function() {
-        document.getElementById(this.id).onclick = function(event) { /* alert("hallo" + event.target.id) */ }
-    }
+}
+
+Choice.create = function(choice) {
+    return "<label id=\"" + choice.id + "label\" for=\"" + choice.id + "\"><input type=\"radio\" id=\"" + choice.id + "\", name=\"choice\" />" + choice.text + "</label>"
+}
+Choice.bind = function(choice) {
+    document.getElementById(choice.id).onclick = function(e) { /* alert("hallo" + event.target.id) */ }
 }
--- game.js
+++ game.js
@@ -1,114 +1,132 @@
-var eventFirst = function() {
-    put("magic", 20)
-    put("lore", 20)
-    put("awareness", 20)
-    put("mundane", 20)
+var e
+var state
 
-    addText("Mit geweiteten Augen starrt sie ihn an:„Was... was... soll das heißen? Dämonen?“ Sie lacht, etwas zu schrill. Er erwidert ihren Blick ruhig: „Lilly... ich bin ein Magier.“ Wieder lacht sie und fährt sich mit der Hand durch die blonden Locken: \"Oh, ach so. Mach dir keine Sorgen, ich bin eine Fee...\"Der Mann verzieht noch immer keine Miene: „Ich gehöre einem uralten Geheimbund an, der die Menschen vor den Mächten der Finsternis bewahrt!“<br/>An dieser Stelle verdrehst du die Augen und wechselst den Kanal. Um diese Uhrzeit kommt einfach kein brauchbarer Film mehr im Fernsehen. Wie konnte so ein Mist jemals gedreht werden? Ein uralter Geheimbund von Magiern... lächerlich. Zugegeben, der Film scheint aus den Achtzigern zu stammen. Damals ahnte man noch nicht, dass bald Internet und Smartphone erfunden und die Welt für immer verändern würde. Mit dem Aufkommen der neuen Kommunikationstechnologie war es völlig unmöglich geworden, die Existenz der Magie noch länger geheim zu halten. Plötzlich tauchten immer mehr Videos von fliegenden Menschen, kreischenden Kugelblitzen und spontan auftretender Dunkelheit auf. Die Öffentlichkeit war ebenso überfordert wie die Medien und Politiker, und in der daraus resultierenden Unruhe wurden Magier gesetzlich verpflichtet, sich zu registrieren und ihre Geheimnisse offen zu legen. Zugleich wurde den meisten eine gutbezahlte Dienststelle angeboten. Die Welt schien sich nur noch um Magie zu drehen.<br />Dir war das egal. Dein Vater war gerade gestorben.")
-    
-    addBottom([new Choice("Weiter", event0_2)])
+function State() {
+    GameState.apply(this, arguments)
+    this.gender = 1
+    this.magic = 20
+    this.lore =  20
+    this.awareness = 20
+    this.mundane =20
 }
 
-var event0_2 = function() { 
-    addText("Ein Autounfall... die Banalität war beinahe ebenso verstörend wie die plötzliche Leere in dir. Während sich deine Mutter dem Alkohol ergab, tatest du das einzige, was dir sinnvoll erschien: Soviel wie möglich zu unternehmen und so wenig wie möglich zu denken. Nichts war dumm genug, kein Aufwand zu groß, sofern du nur nicht mit deinen Erinnerungen alleine warst. Selbst die Tapete deines Zimmer runterzureißen war dir ein Vergnügen. Du hattest sie eh nie gemocht, sie entsprach gängigen Klischees über Jungs und Mädchen und war bei deiner Geburt entsprechend ausgewählt ausgewählt worden:")
-    addBottom([
-        new Choice("Blau", event0_3_1),
-        new Choice("Rosa", event0_3_2)])
+var eventFirst = function() {
+    state = new State()
+    e = new Engine(state)
+    Engine.show("Aiur",
+        [new Choice("Neu", "eventNew"),
+        new Choice("Continue", "eventContinue")], false)
+}
+
+var eventNew = function() {
+    Engine.show("Mit geweiteten Augen starrt sie ihn an:„Was... was... soll das heißen? Dämonen?“ Sie lacht, etwas zu schrill. Er erwidert ihren Blick ruhig: „Lilly... ich bin ein Magier.“ Wieder lacht sie und fährt sich mit der Hand durch die blonden Locken: \"Oh, ach so. Mach dir keine Sorgen, ich bin eine Fee...\"Der Mann verzieht noch immer keine Miene: „Ich gehöre einem uralten Geheimbund an, der die Menschen vor den Mächten der Finsternis bewahrt!“<br/>An dieser Stelle verdrehst du die Augen und wechselst den Kanal. Um diese Uhrzeit kommt einfach kein brauchbarer Film mehr im Fernsehen. Wie konnte so ein Mist jemals gedreht werden? Ein uralter Geheimbund von Magiern... lächerlich. Zugegeben, der Film scheint aus den Achtzigern zu stammen. Damals ahnte man noch nicht, dass bald Internet und Smartphone erfunden und die Welt für immer verändern würde. Mit dem Aufkommen der neuen Kommunikationstechnologie war es völlig unmöglich geworden, die Existenz der Magie noch länger geheim zu halten. Plötzlich tauchten immer mehr Videos von fliegenden Menschen, kreischenden Kugelblitzen und spontan auftretender Dunkelheit auf. Die Öffentlichkeit war ebenso überfordert wie die Medien und Politiker, und in der daraus resultierenden Unruhe wurden Magier gesetzlich verpflichtet, sich zu registrieren und ihre Geheimnisse offen zu legen. Zugleich wurde den meisten eine gutbezahlte Dienststelle angeboten. Die Welt schien sich nur noch um Magie zu drehen.<br />Dir war das egal. Dein Vater war gerade gestorben.",
+        [new Choice("Weiter", "event0_2")])
+}
+
+var eventContinue = function() {
+    GameState.load()
+}
+
+var event0_2 = function() {
+    Engine.show(
+        "Ein Autounfall... die Banalität war beinahe ebenso verstörend wie die plötzliche Leere in dir. Während sich deine Mutter dem Alkohol ergab, tatest du das einzige, was dir sinnvoll erschien: Soviel wie möglich zu unternehmen und so wenig wie möglich zu denken. Nichts war dumm genug, kein Aufwand zu groß, sofern du nur nicht mit deinen Erinnerungen alleine warst. Selbst die Tapete deines Zimmer runterzureißen war dir ein Vergnügen. Du hattest sie eh nie gemocht, sie entsprach gängigen Klischees über Jungs und Mädchen und war bei deiner Geburt entsprechend ausgewählt ausgewählt worden:",
+        [new Choice("Blau", "event0_3_1"),
+        new Choice("Rosa", "event0_3_2")])
 }
 
 var event0_3_1 = function() {
-    put("gender", 1)
-    addText("Blau für Jungs. Und dann auch noch so ein hässlicher Farbton.")
-    addBottom([new Choice("Weiter", event0_3_3)])
+    state.gender = 1
+    Engine.show(
+        "Blau für Jungs. Und dann auch noch so ein hässlicher Farbton.",
+        [new Choice("Weiter", "event0_3_3")])
 }
 
 var event0_3_2 = function() {
-    put("gender", 0)
-    addText("Rosa für Mädchen. Und dann auch noch so ein hässlicher Farbton.")
-    addBottom([new Choice("Weiter", event0_3_3)])
+    state.gender = 0
+    Engine.show("Rosa für Mädchen. Und dann auch noch so ein hässlicher Farbton.",
+    [new Choice("Weiter", "event0_3_3")])
 }
 
 var event0_3_3 = function() {
-    addText("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.")
-    addBottom([new Choice("Weiter", event0_4)])
+    Engine.show("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.", 
+    [new Choice("Weiter", "event0_4")])
 }
 
 var event0_3_3 = function() {
-    addText("Allerdings verbrachtest du auch viel Zeit bei deiner besten Freundin Alex. Neben unzähligen Filmen habt ihr euch vor allem viele Videos im Internet angesehen. Eines Tages grinste sie dich an, während sie zwei bedruckte Seiten Papier vor deine Nase hielt: \"Hier, das hab ich aus 'nem Forum. Der Initiationsritus.\"Der was?\", hattest du sie verwirrt gefragt, nicht ganz sicher, welcher Film das sein sollte.<br/>\"Initiationsritus. Das machen angeblich alle, die Magie erlernen wollen. Damit findet man heraus, ob man magisch begabt ist.\"<br/>Zwanzig Minuten später saßest du ihr gegenüber im Schneidersitz, in der Hand eine brennende Kerze, der Geruch von Weihrauch in der Luft, und lächelteste sie spöttisch an: \"Und, ist die Macht schon mit dir?\"<br/>Alex streckte dir die Zunge raus: \"Magie ist total selten. Dass heißt nicht, dass mein Ritual...\" cooler Effekt: Kerze schmilzt nicht und sondert keine Hitze ab, lässt sich nicht auspusten.<br/>Diese Erkenntnis gab dir neuen Schwung und veränderte dein Leben. Alex sammelte alles, was sie im Internet fand und half dir, dein Talent zu entwickeln. Eine Sache war dir dabei besonders wichtig:")
-    addBottom([
-        new Choice("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.", event0_4_1),
-        new Choice("Ich verschlang jedes Buch und jeden Hinweis voller Wissensdurst. Ich wollte jeden Zauber kennen und jedes Geheimnis ergründen.", event0_4_2),
-        new Choice("Ich gewöhnte mir an, auf jedes Detail in meiner Umgebung zu achten und keine Spuren zu hinterlassen.", event0_4_3),
-        new Choice("Ich achtete darauf, die Welt um mich herum nicht zu vergessen, und verbrachte viel Zeit mit Freunden und sportlichen Aktivitäten.", event0_4_4)
+    Engine.show("Allerdings verbrachtest du auch viel Zeit bei deiner besten Freundin Alex. Neben unzähligen Filmen habt ihr euch vor allem viele Videos im Internet angesehen. Eines Tages grinste sie dich an, während sie zwei bedruckte Seiten Papier vor deine Nase hielt: \"Hier, das hab ich aus 'nem Forum. Der Initiationsritus.\"Der was?\", hattest du sie verwirrt gefragt, nicht ganz sicher, welcher Film das sein sollte.<br/>\"Initiationsritus. Das machen angeblich alle, die Magie erlernen wollen. Damit findet man heraus, ob man magisch begabt ist.\"<br/>Zwanzig Minuten später saßest du ihr gegenüber im Schneidersitz, in der Hand eine brennende Kerze, der Geruch von Weihrauch in der Luft, und lächelteste sie spöttisch an: \"Und, ist die Macht schon mit dir?\"<br/>Alex streckte dir die Zunge raus: \"Magie ist total selten. Dass heißt nicht, dass mein Ritual...\" cooler Effekt: Kerze schmilzt nicht und sondert keine Hitze ab, lässt sich nicht auspusten.<br/>Diese Erkenntnis gab dir neuen Schwung und veränderte dein Leben. Alex sammelte alles, was sie im Internet fand und half dir, dein Talent zu entwickeln. Eine Sache war dir dabei besonders wichtig:",
+    [
+        new Choice("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.", "event0_4_1"),
+        new Choice("Ich verschlang jedes Buch und jeden Hinweis voller Wissensdurst. Ich wollte jeden Zauber kennen und jedes Geheimnis ergründen.", "event0_4_2"),
+        new Choice("Ich gewöhnte mir an, auf jedes Detail in meiner Umgebung zu achten und keine Spuren zu hinterlassen.", "event0_4_3"),
+        new Choice("Ich achtete darauf, die Welt um mich herum nicht zu vergessen, und verbrachte viel Zeit mit Freunden und sportlichen Aktivitäten.", "event0_4_4")
     ])
 }
 
 var event0_4_1 = function() {
-    put("magic", get("magic") + 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_4_5)])
+    state.magic += 10
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_2 = function() {
-    put("lore", get("lore") + 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_4_5)])
+    state.lore += 10
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_3 = function() {
-    put("awareness", get("awareness") + 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_4_5)])
+    state.awareness += 10
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_4 = function() {
-    put("mundane", get("mundane") + 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_4_5)])
+    state.mundane += 10
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_5 = function() {
-    addText("Deine Begeisterung erhielt erst einen Dämpfer, als Thomas Brendel erschossen wurde. Brendel war einer jener Magierbeamten der ersten Stunde, und war für den Schutz der Regierung zuständig gewesen. Ermittler hatten herausgefunden, dass er seine Kräfte genutzt hatte, um hochrangige Politiker zu beeinflussen. Bei seiner Verhaftung hatte er ein Dutztend Sondereinsatzkommandos in Schlacke verwandelt, bevor eine Kugel seinen Kopf durchbohrte. Angesichts dieses gravierenden Machtmissbrauches brandete die öffentliche Panik erneut auf, was für registrierte Magier ernsthafte Folgen hatte: Magie durfte nur noch mit Genehmigung der Behörden und nur in bestimmten Situationen genutzt werden, sofern keine Vorstrafen vorhanden waren. Jeder gemeldete Magier musste unregelmäßige Zufallskontrollen über sich ergehen lassen, unabhängig davon, ob er Magie ausübte oder nicht. Das schlimmste jedoch waren die radikalen Gruppen besorgter Bürger, welche sämtliche Magier als Schwerkriminelle behandelte und auch vor Gewalt nicht zurückschreckten.<br/>Für dich war dies besonders schrecklich. Da ihr den Großteil eurer Quellen aus dem sogenanntem Darknet auf illegalen Tauschbörsen erworben hattet, standen die Chancen gut, dass dir die Ausübung von Magie verboten werden würde. Dabei gab es streng genommen kein Gesetz, dass dies bislang untersagt hatte, doch einige Gerichtsurteile änderten dies schnell. Statt einer gesicherten Laufbahn sahest du dich nun dem Risiko von gewalttätigen Anfeindungen und staatlicher Überwachung ausgesetzt. Es muss nicht erwähnt werden, dass dies entmutigend war.")
-    addBottom([
-        new Choice("Ich ignorierte meine magischen Kräfte für eine Weile.", event0_5_1),
-        new Choice("Ich vernachlässigte die magischen Schriften.", event0_5_2),
-        new Choice("Ich schenkte meiner Umgebung keine Aufmerksamkeit mehr.", event0_5_3),
-        new Choice("Ich verkroch mich in meinem Zimmer.", event0_5_4)
+    Engine.show("Deine Begeisterung erhielt erst einen Dämpfer, als Thomas Brendel erschossen wurde. Brendel war einer jener Magierbeamten der ersten Stunde, und war für den Schutz der Regierung zuständig gewesen. Ermittler hatten herausgefunden, dass er seine Kräfte genutzt hatte, um hochrangige Politiker zu beeinflussen. Bei seiner Verhaftung hatte er ein Dutztend Sondereinsatzkommandos in Schlacke verwandelt, bevor eine Kugel seinen Kopf durchbohrte. Angesichts dieses gravierenden Machtmissbrauches brandete die öffentliche Panik erneut auf, was für registrierte Magier ernsthafte Folgen hatte: Magie durfte nur noch mit Genehmigung der Behörden und nur in bestimmten Situationen genutzt werden, sofern keine Vorstrafen vorhanden waren. Jeder gemeldete Magier musste unregelmäßige Zufallskontrollen über sich ergehen lassen, unabhängig davon, ob er Magie ausübte oder nicht. Das schlimmste jedoch waren die radikalen Gruppen besorgter Bürger, welche sämtliche Magier als Schwerkriminelle behandelte und auch vor Gewalt nicht zurückschreckten.<br/>Für dich war dies besonders schrecklich. Da ihr den Großteil eurer Quellen aus dem sogenanntem Darknet auf illegalen Tauschbörsen erworben hattet, standen die Chancen gut, dass dir die Ausübung von Magie verboten werden würde. Dabei gab es streng genommen kein Gesetz, dass dies bislang untersagt hatte, doch einige Gerichtsurteile änderten dies schnell. Statt einer gesicherten Laufbahn sahest du dich nun dem Risiko von gewalttätigen Anfeindungen und staatlicher Überwachung ausgesetzt. Es muss nicht erwähnt werden, dass dies entmutigend war.",
+    [
+        new Choice("Ich ignorierte meine magischen Kräfte für eine Weile.", "event0_5_1"),
+        new Choice("Ich vernachlässigte die magischen Schriften.", "event0_5_2"),
+        new Choice("Ich schenkte meiner Umgebung keine Aufmerksamkeit mehr.", "event0_5_3"),
+        new Choice("Ich verkroch mich in meinem Zimmer.", "event0_5_4")
     ])
 }
 
 var event0_5_1 = function() {
-    put("magic", get("magic") - 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_5_5)])
+    state.magic -= 10
+    Engine.show("TODO: Beschreiben was passiert", 
+    [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_2 = function() {
-    put("lore", get("lore") - 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_5_5)])
+    state.lore -= 10
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_3 = function() {
-    put("awareness", get("awareness") - 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_5_5)])
+    state.awareness -= 10
+    Engine.show("TODO: Beschreiben was passiert", 
+    [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_4 = function() {
-    put("mundane", get("mundane") - 10)
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", event0_5_5)])
+    state.mundane -= 10
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_5 = function() {
-    addText("TODO: Beschreiben was passiert")
-    addBottom([new Choice("Weiter", eventEnd)])
+    Engine.show("TODO: Beschreiben was passiert",
+    [new Choice("Weiter", "eventEnd")])
 }
 
 var eventEnd = function() {
-    addText("The End")
-    addBottom()
+    Engine.show("The End")
 }
 
 window.onload = eventFirst;
