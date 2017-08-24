8ccccad8e70a01c346502f8d9ecf44a1198d8a90
fdsa fdsa <redwolf2@gmx.de>
2017-08-24 23:36:57 +0200

    Added PlayerAttributes to show them later

--- engine.js
+++ engine.js
@@ -35,80 +35,86 @@ function Engine(customState)
     var clearText = function() {
         addText("")
     }
-}
 
-Engine.show = function(text, choices, save = true) {
-    Engine.addText(text)
-    Engine.addBottom(choices)
-    gamestate.event = new GameEvent(text, choices)
-    if(save)
-        GameState.save()
-}
-
-Engine.createChoices = function(choices) {
-    var ret = ""
-    choices.forEach(function(choice) {
-        ret += (Choice.create(choice) + "<br/><br/>")
-    })
-    return ret
-}
-
-Engine.addBottom = function(choices) {
-    globalChoices = choices
-    var e = document.getElementById("center")
-    if(choices === undefined) {
-        e.innerHTML = ""
-    } else if(choices.length > 1) {
-        var counter = 0
+    createChoices = function(choices) {
+        var ret = ""
         choices.forEach(function(choice) {
-            counter++
-            choice.id = "choice" + counter
+            ret += (Choice.create(choice) + "<br/><br/>")
         })
-        e.innerHTML = Engine.createChoices(choices) 
-        choices.forEach(function(choice) {
-            Choice.bind(choice)
-        })
-        e.innerHTML += Engine.createButton("confirm", "Weiter", function() {})
-    } else if(choices.length === 1) {
-        e.innerHTML = Engine.createButton(choices[0].id, "Weiter", function() {})
+        return ret
     }
-}
 
-Engine.createButton = function(id, text, func) {
-    return "<input type=\"button\" id=\"" + id + "\", name=\"button\" class=\"btn continue\", onclick=\"Engine.onButton(" + id + ")\", value=\"" + text +  "\" />"
-}
+    addBottom = function(choices) {
+        globalChoices = choices
+        var e = document.getElementById("center")
+        if(choices === undefined) {
+            e.innerHTML = ""
+            return;
+        } else if(choices.length > 1) {
+            var counter = 0
+            choices.forEach(function(choice) {
+                counter++
+                choice.id = "choice" + counter
+            })
+            e.innerHTML = createChoices(choices) 
+            choices.forEach(function(choice) {
+                Choice.bind(choice)
+            })
+        }
+        var id = choices.length === 1 ? "confirm" : choices[0].id
+        var text = choices.length === 1 ? choices[0].text : "Weiter"
+        var append = choices.length !== 1
+        insertButton(e, "confirm", text, function() { onButton("" + id) }, "btn continue", append)
+    }
 
-Engine.addText = function(text) {
-    document.getElementById("text").innerHTML = text
-}
+    insertButton = function(e, id, text, onclick, clazz = "btn", append = true) {
+        var buttonHtml = "<input type=\"button\" id=\"" + id + "\", name=\"button\" class=\"" + clazz + "\", value=\"" + text +  "\" />"
+        if(append)
+            e.innerHTML += buttonHtml
+        else
+            e.innerHTML = buttonHtml
+        document.getElementById(id).onclick = function(id) { onclick(id) }
+    }
 
-Engine.getSelectedChoice = function() {
-    var selectedId = undefined
-    if(globalChoices.length > 1) {
-        var choices = document.getElementsByName("choice")
-        choices.forEach(function(choice) {
-            if(choice.checked) {
-                selectedId = choice.id
+    addText = function(text) {
+        document.getElementById("text").innerHTML = text
+    }
+
+    getSelectedChoice = function() {
+        var selectedId = undefined
+        if(globalChoices.length > 1) {
+            var choices = document.getElementsByName("choice")
+            choices.forEach(function(choice) {
+                if(choice.checked) {
+                    selectedId = choice.id
+                }
+            })
+        } else if(globalChoices.length === 1) {
+            selectedId = globalChoices[0].id
+        }
+        return selectedId
+    }
+
+    onButton = function(button) {
+        var selectedId = getSelectedChoice()
+        var selectedChoice
+        globalChoices.forEach(function(choice) {
+            if(choice.id === selectedId) {
+                selectedChoice = choice
             }
         })
-    } else if(globalChoices.length === 1) {
-        selectedId = globalChoices[0].id
-    }
-    return selectedId
-}
-
-Engine.onButton = function(button) {
-    var selectedId = Engine.getSelectedChoice()
-    var selectedChoice
-    globalChoices.forEach(function(choice) {
-        if(choice.id === selectedId) {
-            selectedChoice = choice
+        if(selectedChoice != undefined) {
+            console.log(selectedChoice.text + " was selected.")
+            window[selectedChoice.func](arguments)
+            window.scrollTo(0, 0)
         }
-    })
-    if(selectedChoice != undefined) {
-        console.log(selectedChoice.text + " was selected.")
-        window[selectedChoice.func](arguments)
-        window.scrollTo(0, 0)
+    }
+    this.show = function(text, choices, save = true) {
+        addText(text)
+        addBottom(choices)
+        gamestate.event = new GameEvent(text, choices)
+        if(save)
+            GameState.save()
     }
 }
 
@@ -116,12 +122,18 @@ function GameState() {
     this.event = new GameEvent("", [new Choice("Weiter", "event404")])
 }
 
+function PlayerAttribute(name, value) {
+    this.name = name
+    this.value = value
+    this.visible = true
+}
+
 GameState.save = function() {
     put("gamestate", gamestate)
 }
-GameState.load = function() {
+GameState.load = function(engine) {
     gamestate = get("gamestate")
-    Engine.show(gamestate.event.text, gamestate.event.choices)
+    engine.show(gamestate.event.text, gamestate.event.choices)
 }
 
 function GameEvent(text, choices) {
@@ -138,6 +150,7 @@ function Choice(text, func) {
 Choice.create = function(choice) {
     return "<label id=\"" + choice.id + "label\" for=\"" + choice.id + "\"><input type=\"radio\" id=\"" + choice.id + "\", name=\"choice\" />" + choice.text + "</label>"
 }
+
 Choice.bind = function(choice) {
-    document.getElementById(choice.id).onclick = function(e) { /* alert("hallo" + event.target.id) */ }
+    //document.getElementById(choice.id).onclick = function(e) { /* alert("hallo" + event.target.id) */ }
 }
--- game.js
+++ game.js
@@ -4,31 +4,31 @@ var state
 function State() {
     GameState.apply(this, arguments)
     this.gender = 1
-    this.magic = 20
-    this.lore =  20
-    this.awareness = 20
-    this.mundane =20
+    this.magic = new PlayerAttribute("Magie", 20)
+    this.lore = new PlayerAttribute("Wissen", 20)
+    this.awareness = new PlayerAttribute("Aufmerksamkeit", 20)
+    this.mundane = new PlayerAttribute("Weltliches", 20)
 }
 
 var eventFirst = function() {
     state = new State()
     e = new Engine(state)
-    Engine.show("Aiur",
+    e.show("Aiur",
         [new Choice("Neu", "eventNew"),
         new Choice("Continue", "eventContinue")], false)
 }
 
 var eventNew = function() {
-    Engine.show("Mit geweiteten Augen starrt sie ihn an:„Was... was... soll das heißen? Dämonen?“ Sie lacht, etwas zu schrill. Er erwidert ihren Blick ruhig: „Lilly... ich bin ein Magier.“ Wieder lacht sie und fährt sich mit der Hand durch die blonden Locken: \"Oh, ach so. Mach dir keine Sorgen, ich bin eine Fee...\"Der Mann verzieht noch immer keine Miene: „Ich gehöre einem uralten Geheimbund an, der die Menschen vor den Mächten der Finsternis bewahrt!“<br/>An dieser Stelle verdrehst du die Augen und wechselst den Kanal. Um diese Uhrzeit kommt einfach kein brauchbarer Film mehr im Fernsehen. Wie konnte so ein Mist jemals gedreht werden? Ein uralter Geheimbund von Magiern... lächerlich. Zugegeben, der Film scheint aus den Achtzigern zu stammen. Damals ahnte man noch nicht, dass bald Internet und Smartphone erfunden und die Welt für immer verändern würde. Mit dem Aufkommen der neuen Kommunikationstechnologie war es völlig unmöglich geworden, die Existenz der Magie noch länger geheim zu halten. Plötzlich tauchten immer mehr Videos von fliegenden Menschen, kreischenden Kugelblitzen und spontan auftretender Dunkelheit auf. Die Öffentlichkeit war ebenso überfordert wie die Medien und Politiker, und in der daraus resultierenden Unruhe wurden Magier gesetzlich verpflichtet, sich zu registrieren und ihre Geheimnisse offen zu legen. Zugleich wurde den meisten eine gutbezahlte Dienststelle angeboten. Die Welt schien sich nur noch um Magie zu drehen.<br />Dir war das egal. Dein Vater war gerade gestorben.",
+    e.show("Mit geweiteten Augen starrt sie ihn an:„Was... was... soll das heißen? Dämonen?“ Sie lacht, etwas zu schrill. Er erwidert ihren Blick ruhig: „Lilly... ich bin ein Magier.“ Wieder lacht sie und fährt sich mit der Hand durch die blonden Locken: \"Oh, ach so. Mach dir keine Sorgen, ich bin eine Fee...\"Der Mann verzieht noch immer keine Miene: „Ich gehöre einem uralten Geheimbund an, der die Menschen vor den Mächten der Finsternis bewahrt!“<br/>An dieser Stelle verdrehst du die Augen und wechselst den Kanal. Um diese Uhrzeit kommt einfach kein brauchbarer Film mehr im Fernsehen. Wie konnte so ein Mist jemals gedreht werden? Ein uralter Geheimbund von Magiern... lächerlich. Zugegeben, der Film scheint aus den Achtzigern zu stammen. Damals ahnte man noch nicht, dass bald Internet und Smartphone erfunden und die Welt für immer verändern würde. Mit dem Aufkommen der neuen Kommunikationstechnologie war es völlig unmöglich geworden, die Existenz der Magie noch länger geheim zu halten. Plötzlich tauchten immer mehr Videos von fliegenden Menschen, kreischenden Kugelblitzen und spontan auftretender Dunkelheit auf. Die Öffentlichkeit war ebenso überfordert wie die Medien und Politiker, und in der daraus resultierenden Unruhe wurden Magier gesetzlich verpflichtet, sich zu registrieren und ihre Geheimnisse offen zu legen. Zugleich wurde den meisten eine gutbezahlte Dienststelle angeboten. Die Welt schien sich nur noch um Magie zu drehen.<br />Dir war das egal. Dein Vater war gerade gestorben.",
         [new Choice("Weiter", "event0_2")])
 }
 
 var eventContinue = function() {
-    GameState.load()
+    GameState.load(e)
 }
 
 var event0_2 = function() {
-    Engine.show(
+    e.show(
         "Ein Autounfall... die Banalität war beinahe ebenso verstörend wie die plötzliche Leere in dir. Während sich deine Mutter dem Alkohol ergab, tatest du das einzige, was dir sinnvoll erschien: Soviel wie möglich zu unternehmen und so wenig wie möglich zu denken. Nichts war dumm genug, kein Aufwand zu groß, sofern du nur nicht mit deinen Erinnerungen alleine warst. Selbst die Tapete deines Zimmer runterzureißen war dir ein Vergnügen. Du hattest sie eh nie gemocht, sie entsprach gängigen Klischees über Jungs und Mädchen und war bei deiner Geburt entsprechend ausgewählt ausgewählt worden:",
         [new Choice("Blau", "event0_3_1"),
         new Choice("Rosa", "event0_3_2")])
@@ -36,24 +36,24 @@ var event0_2 = function() {
 
 var event0_3_1 = function() {
     state.gender = 1
-    Engine.show(
+    e.show(
         "Blau für Jungs. Und dann auch noch so ein hässlicher Farbton.",
         [new Choice("Weiter", "event0_3_3")])
 }
 
 var event0_3_2 = function() {
     state.gender = 0
-    Engine.show("Rosa für Mädchen. Und dann auch noch so ein hässlicher Farbton.",
+    e.show("Rosa für Mädchen. Und dann auch noch so ein hässlicher Farbton.",
     [new Choice("Weiter", "event0_3_3")])
 }
 
 var event0_3_3 = function() {
-    Engine.show("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.", 
+    e.show("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.", 
     [new Choice("Weiter", "event0_4")])
 }
 
 var event0_3_3 = function() {
-    Engine.show("Allerdings verbrachtest du auch viel Zeit bei deiner besten Freundin Alex. Neben unzähligen Filmen habt ihr euch vor allem viele Videos im Internet angesehen. Eines Tages grinste sie dich an, während sie zwei bedruckte Seiten Papier vor deine Nase hielt: \"Hier, das hab ich aus 'nem Forum. Der Initiationsritus.\"Der was?\", hattest du sie verwirrt gefragt, nicht ganz sicher, welcher Film das sein sollte.<br/>\"Initiationsritus. Das machen angeblich alle, die Magie erlernen wollen. Damit findet man heraus, ob man magisch begabt ist.\"<br/>Zwanzig Minuten später saßest du ihr gegenüber im Schneidersitz, in der Hand eine brennende Kerze, der Geruch von Weihrauch in der Luft, und lächelteste sie spöttisch an: \"Und, ist die Macht schon mit dir?\"<br/>Alex streckte dir die Zunge raus: \"Magie ist total selten. Dass heißt nicht, dass mein Ritual...\" cooler Effekt: Kerze schmilzt nicht und sondert keine Hitze ab, lässt sich nicht auspusten.<br/>Diese Erkenntnis gab dir neuen Schwung und veränderte dein Leben. Alex sammelte alles, was sie im Internet fand und half dir, dein Talent zu entwickeln. Eine Sache war dir dabei besonders wichtig:",
+    e.show("Allerdings verbrachtest du auch viel Zeit bei deiner besten Freundin Alex. Neben unzähligen Filmen habt ihr euch vor allem viele Videos im Internet angesehen. Eines Tages grinste sie dich an, während sie zwei bedruckte Seiten Papier vor deine Nase hielt: \"Hier, das hab ich aus 'nem Forum. Der Initiationsritus.\"Der was?\", hattest du sie verwirrt gefragt, nicht ganz sicher, welcher Film das sein sollte.<br/>\"Initiationsritus. Das machen angeblich alle, die Magie erlernen wollen. Damit findet man heraus, ob man magisch begabt ist.\"<br/>Zwanzig Minuten später saßest du ihr gegenüber im Schneidersitz, in der Hand eine brennende Kerze, der Geruch von Weihrauch in der Luft, und lächelteste sie spöttisch an: \"Und, ist die Macht schon mit dir?\"<br/>Alex streckte dir die Zunge raus: \"Magie ist total selten. Dass heißt nicht, dass mein Ritual...\" cooler Effekt: Kerze schmilzt nicht und sondert keine Hitze ab, lässt sich nicht auspusten.<br/>Diese Erkenntnis gab dir neuen Schwung und veränderte dein Leben. Alex sammelte alles, was sie im Internet fand und half dir, dein Talent zu entwickeln. Eine Sache war dir dabei besonders wichtig:",
     [
         new Choice("Ich widmete meine gesamte Kraft dem Bündeln magischer Energien, um möglichst mächtige Zauber zu formen.", "event0_4_1"),
         new Choice("Ich verschlang jedes Buch und jeden Hinweis voller Wissensdurst. Ich wollte jeden Zauber kennen und jedes Geheimnis ergründen.", "event0_4_2"),
@@ -63,31 +63,31 @@ var event0_3_3 = function() {
 }
 
 var event0_4_1 = function() {
-    state.magic += 10
-    Engine.show("Du verbrachtest unzählige Stunden damit, dir Spielkarten zu merken, um deine Konzentration zu schärfen. Jeden Tag meditiertest du, um das zu finden, was die Bücher als Ruhepol bezeichneten. Und jeder dir bekannte Zauber wurde in den unterschiedlichsten Varianten probiert, bis du dessen Wirkung besser einschätzen konntest. Stück für Stück bautest du dein Vertrauen in die Magie weiter aus, bis sie für dich weniger ein Werkzeug als eine dritte Hand darstellte.",
+    state.magic.value += 10
+    e.show("Du verbrachtest unzählige Stunden damit, dir Spielkarten zu merken, um deine Konzentration zu schärfen. Jeden Tag meditiertest du, um das zu finden, was die Bücher als Ruhepol bezeichneten. Und jeder dir bekannte Zauber wurde in den unterschiedlichsten Varianten probiert, bis du dessen Wirkung besser einschätzen konntest. Stück für Stück bautest du dein Vertrauen in die Magie weiter aus, bis sie für dich weniger ein Werkzeug als eine dritte Hand darstellte.",
     [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_2 = function() {
-    state.lore += 10
-    Engine.show("Du hattest zwar zuvor schon viele Bücher gelesen, aber die magischen Schriften eröffneten dir eine völlig neue Weltanschauung. Physik, Chemie und Biologie verloren ihren unnachgiebigen Halt und wurden zu alternativen Erklärungsmodellen, während Legenden zu Optionen wurden. Inzwischen hast du mehr Fragen als jemals zuvor, doch zugleich hast du das Gefühl, eine Orientierung im Chaos gefunden zu haben.",
+    state.lore.value += 10
+    e.show("Du hattest zwar zuvor schon viele Bücher gelesen, aber die magischen Schriften eröffneten dir eine völlig neue Weltanschauung. Physik, Chemie und Biologie verloren ihren unnachgiebigen Halt und wurden zu alternativen Erklärungsmodellen, während Legenden zu Optionen wurden. Inzwischen hast du mehr Fragen als jemals zuvor, doch zugleich hast du das Gefühl, eine Orientierung im Chaos gefunden zu haben.",
     [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_3 = function() {
-    state.awareness += 10
-    Engine.show("Die Tatsache, dass etwas so Bedeutsames wie Magie jemals ignoriert werden konnte, obwohl sie doch in den Geschichten jeder Kultur auftauchte, machte dir klar, wie leicht selbst Offensichtliches übersehen werden kann. Mit dem festen Vorsatz, nie wieder mit verschlossenen Augen durch die Welt zu laufen, machtest du es dir zur Gewohnheit, auf die Details deiner Umgebung zu achten, diese in einem Gedächtnisprotokoll festzuhalten und sie schließlich miteinander zu vergleichen.",
+    state.awareness.value += 10
+    e.show("Die Tatsache, dass etwas so Bedeutsames wie Magie jemals ignoriert werden konnte, obwohl sie doch in den Geschichten jeder Kultur auftauchte, machte dir klar, wie leicht selbst Offensichtliches übersehen werden kann. Mit dem festen Vorsatz, nie wieder mit verschlossenen Augen durch die Welt zu laufen, machtest du es dir zur Gewohnheit, auf die Details deiner Umgebung zu achten, diese in einem Gedächtnisprotokoll festzuhalten und sie schließlich miteinander zu vergleichen.",
     [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_4 = function() {
-    state.mundane += 10
-    Engine.show("Schwimmen und Radfahren halfen dir, den Kopf frei zu bekommen, und zugleich gaben sie dir das Gefühl, auch ohne Magie etwas erreichen zu können. Der Kontakt mit Freundesfreunden führte zu interessanten Begegnungen und unterhaltsamen Gesprächen bis in den späten Abend, welche nicht nur deinen Alltag bereicherten, sondern es dir auch ermöglichten, die Magie zu vergessen und zu entspannen. Obwohl die Magie dein Leben noch immer prägte, war es gut, eine Alternative zu haben.",
+    state.mundane.value += 10
+    e.show("Schwimmen und Radfahren halfen dir, den Kopf frei zu bekommen, und zugleich gaben sie dir das Gefühl, auch ohne Magie etwas erreichen zu können. Der Kontakt mit Freundesfreunden führte zu interessanten Begegnungen und unterhaltsamen Gesprächen bis in den späten Abend, welche nicht nur deinen Alltag bereicherten, sondern es dir auch ermöglichten, die Magie zu vergessen und zu entspannen. Obwohl die Magie dein Leben noch immer prägte, war es gut, eine Alternative zu haben.",
     [new Choice("Weiter", "event0_4_5")])
 }
 
 var event0_4_5 = function() {
-    Engine.show("Deine Begeisterung erhielt erst einen Dämpfer, als Thomas Brendel erschossen wurde. Brendel war einer jener Magierbeamten der ersten Stunde, und war für den Schutz der Regierung zuständig gewesen. Ermittler hatten herausgefunden, dass er seine Kräfte genutzt hatte, um hochrangige Politiker zu beeinflussen. Bei seiner Verhaftung hatte er ein Dutztend Sondereinsatzkommandos in Schlacke verwandelt, bevor eine Kugel seinen Kopf durchbohrte. Angesichts dieses gravierenden Machtmissbrauches brandete die öffentliche Panik erneut auf, was für registrierte Magier ernsthafte Folgen hatte: Magie durfte nur noch mit Genehmigung der Behörden und nur in bestimmten Situationen genutzt werden, sofern keine Vorstrafen vorhanden waren. Jeder gemeldete Magier musste unregelmäßige Zufallskontrollen über sich ergehen lassen, unabhängig davon, ob er Magie ausübte oder nicht. Das schlimmste jedoch waren die radikalen Gruppen besorgter Bürger, welche sämtliche Magier als Schwerkriminelle behandelte und auch vor Gewalt nicht zurückschreckten.<br/>Für dich war dies besonders schrecklich. Da ihr den Großteil eurer Quellen aus dem sogenanntem Darknet auf illegalen Tauschbörsen erworben hattet, standen die Chancen gut, dass dir die Ausübung von Magie verboten werden würde. Dabei gab es streng genommen kein Gesetz, dass dies bislang untersagt hatte, doch einige Gerichtsurteile änderten dies schnell. Statt einer gesicherten Laufbahn sahest du dich nun dem Risiko von gewalttätigen Anfeindungen und staatlicher Überwachung ausgesetzt. Es muss nicht erwähnt werden, dass dies entmutigend war.",
+    e.show("Deine Begeisterung erhielt erst einen Dämpfer, als Thomas Brendel erschossen wurde. Brendel war einer jener Magierbeamten der ersten Stunde, und war für den Schutz der Regierung zuständig gewesen. Ermittler hatten herausgefunden, dass er seine Kräfte genutzt hatte, um hochrangige Politiker zu beeinflussen. Bei seiner Verhaftung hatte er ein Dutztend Sondereinsatzkommandos in Schlacke verwandelt, bevor eine Kugel seinen Kopf durchbohrte. Angesichts dieses gravierenden Machtmissbrauches brandete die öffentliche Panik erneut auf, was für registrierte Magier ernsthafte Folgen hatte: Magie durfte nur noch mit Genehmigung der Behörden und nur in bestimmten Situationen genutzt werden, sofern keine Vorstrafen vorhanden waren. Jeder gemeldete Magier musste unregelmäßige Zufallskontrollen über sich ergehen lassen, unabhängig davon, ob er Magie ausübte oder nicht. Das schlimmste jedoch waren die radikalen Gruppen besorgter Bürger, welche sämtliche Magier als Schwerkriminelle behandelte und auch vor Gewalt nicht zurückschreckten.<br/>Für dich war dies besonders schrecklich. Da ihr den Großteil eurer Quellen aus dem sogenanntem Darknet auf illegalen Tauschbörsen erworben hattet, standen die Chancen gut, dass dir die Ausübung von Magie verboten werden würde. Dabei gab es streng genommen kein Gesetz, dass dies bislang untersagt hatte, doch einige Gerichtsurteile änderten dies schnell. Statt einer gesicherten Laufbahn sahest du dich nun dem Risiko von gewalttätigen Anfeindungen und staatlicher Überwachung ausgesetzt. Es muss nicht erwähnt werden, dass dies entmutigend war.",
     [
         new Choice("Ich ignorierte meine magischen Kräfte für eine Weile.", "event0_5_1"),
         new Choice("Ich vernachlässigte die magischen Schriften.", "event0_5_2"),
@@ -97,36 +97,36 @@ var event0_4_5 = function() {
 }
 
 var event0_5_1 = function() {
-    state.magic -= 10
-    Engine.show("Eine Zeit lang war es schwierig, sich auf die Magie einzulassen, wo sie doch dein Leben zugleich so durcheinander gebracht hatte und deine Zukunft bedrohte, doch schließlich konntest du dich wieder fangen, auch wenn deine anfängliche Begeisterung einen Dämpfer erhalten hat.", 
+    state.magic.value -= 10
+    e.show("Eine Zeit lang war es schwierig, sich auf die Magie einzulassen, wo sie doch dein Leben zugleich so durcheinander gebracht hatte und deine Zukunft bedrohte, doch schließlich konntest du dich wieder fangen, auch wenn deine anfängliche Begeisterung einen Dämpfer erhalten hat.", 
     [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_2 = function() {
-    state.lore -= 10
-    Engine.show("Das plötzliche Verbot magischer Schriften dämpfte deine Begeisterung für diese ebenso sehr wie die umständlichen Formulierungen, die offensichtlich keinerlei Wert auf Verständlichkeit legten. Aus diesem Grund hast du die Passagen über seltene Phänomene nur überflogen und dich auf die wesentlichen Dinge beschränkt.",
+    state.lore.value -= 10
+    e.show("Das plötzliche Verbot magischer Schriften dämpfte deine Begeisterung für diese ebenso sehr wie die umständlichen Formulierungen, die offensichtlich keinerlei Wert auf Verständlichkeit legten. Aus diesem Grund hast du die Passagen über seltene Phänomene nur überflogen und dich auf die wesentlichen Dinge beschränkt.",
     [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_3 = function() {
-    state.awareness -= 10
-    Engine.show("Ab einem gewissen Punkt sind zu viele Informationen einfach nur noch hinderlich, und so erschien es dir unumgänglich, deinem schmerzenden Kopf eine Auszeit zu gönnen und nicht jedes Detail zu hinterfragen.", 
+    state.awareness.value -= 10
+    e.show("Ab einem gewissen Punkt sind zu viele Informationen einfach nur noch hinderlich, und so erschien es dir unumgänglich, deinem schmerzenden Kopf eine Auszeit zu gönnen und nicht jedes Detail zu hinterfragen.", 
     [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_4 = function() {
-    state.mundane -= 10
-    Engine.show("Die Welt schien sich gegen dich verschworen zu haben, weshalb du dir eine Auszeit von ihr gönntest. In der Abgeschiedenheit deines Zimmers hattest du alle Zeit der Welt, dir deine eigenen Gedanken zu machen, ohne dass dich jemand störte.",
+    state.mundane.value -= 10
+    e.show("Die Welt schien sich gegen dich verschworen zu haben, weshalb du dir eine Auszeit von ihr gönntest. In der Abgeschiedenheit deines Zimmers hattest du alle Zeit der Welt, dir deine eigenen Gedanken zu machen, ohne dass dich jemand störte.",
     [new Choice("Weiter", "event0_5_5")])
 }
 
 var event0_5_5 = function() {
-    Engine.show("TODO: Beschreiben was passiert",
+    e.show("TODO: Beschreiben was passiert",
     [new Choice("Weiter", "eventEnd")])
 }
 
 var eventEnd = function() {
-    Engine.show("The End")
+    e.show("The End")
 }
 
 window.onload = eventFirst;
