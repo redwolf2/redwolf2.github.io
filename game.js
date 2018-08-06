9b291704b78fc47bfab8054b349f5242c709cd41
redwolf <redwolf2@gmx.de>
2018-08-06 22:26:41 +0200

    removed debug

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ function State() {
 }
 
 var eventStart = function() {
-    debug = true
+    debug = false
     state = new State()
     e = new Engine(state)
     e.setBackground(null, "logo.svg")
